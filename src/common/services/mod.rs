3103660792d05c5314a79d4230805b7a0ea699ce
André Luís <zandreluisos@gmail.com>
2025-10-25 20:59:49 -0300

    feat: add interface_declaration_service with interface query methods

--- src/common/services/mod.rs
+++ src/common/services/mod.rs
@@ -2,4 +2,5 @@ pub mod annotation_service;
 pub mod class_declaration_service;
 pub mod field_declaration_service;
 pub mod import_declaration_service;
+pub mod interface_declaration_service;
 pub mod package_declaration_service;
