052e12e97025d18654ca6b1a273a0693577243a0
André Luís <zandreluisos@gmail.com>
2025-11-01 11:26:10 -0300

    refactor: rename command from get-entity-info to get-jpa-entity-info for consistency

--- src/commands/get_jpa_entity_info_command.rs
+++ src/commands/get_jpa_entity_info_command.rs
@@ -11,7 +11,7 @@ pub fn execute(
   b64_source_code: Option<&str>,
 ) -> Response<GetJpaEntityInfoResponse> {
   let cwd_string = cwd.display().to_string();
-  let cmd_name = String::from("get-entity-info");
+  let cmd_name = String::from("get-jpa-entity-info");
   match run(entity_file_path, b64_source_code) {
     Ok(response) => Response::success(cmd_name, cwd_string, response),
     Err(error_msg) => Response::error(cmd_name, cwd_string, error_msg),
