1f843f641f3cece87b9dab9e8ebcc08615597e87
André Luís <zandreluisos@gmail.com>
2025-11-11 14:35:53 -0300

    refactor: rename BasicJavaType to JavaBasicTypesResponse for clarity and consistency
    update type naming to better reflect its purpose and align with project conventions

--- src/responses/basic_java_type_response.rs
+++ src/responses/basic_java_type_response.rs
@@ -2,10 +2,8 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 #[serde(rename_all = "camelCase")]
-pub struct BasicJavaType {
+pub struct JavaBasicTypeResponse {
   pub id: String,
   pub name: String,
-  pub package_path: String,
-  #[serde(rename = "type")]
-  pub type_: String,
+  pub package_path: Option<String>,
 }
