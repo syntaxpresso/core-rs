9b93a423d5a7f070616d5b9f2d8dddf3da645d97
André Luís <zandreluisos@gmail.com>
2025-11-11 14:44:06 -0300

    feat(commands): update getjavabasictypes command with new arguments and response
    add support for new arguments and update the response structure in the getjavabasictypes command to enhance flexibility and improve integration with updated client requirements

--- src/commands/get_java_basic_types_command.rs
+++ src/commands/get_java_basic_types_command.rs
@@ -2,17 +2,17 @@ use std::path::Path;
 
 use crate::{
   commands::services::get_java_basic_types_service::run,
-  common::types::field_types::JavaBasicFieldTypeKind,
-  responses::{basic_java_type::BasicJavaType, response::Response},
+  common::types::basic_java_types::JavaBasicType,
+  responses::{basic_java_type_response::JavaBasicTypeResponse, response::Response},
 };
 
 pub fn execute(
   cwd: &Path,
-  field_type_kind: &JavaBasicFieldTypeKind,
-) -> Response<Vec<BasicJavaType>> {
+  basic_type_kind: &JavaBasicType,
+) -> Response<Vec<JavaBasicTypeResponse>> {
   let cwd_string = cwd.display().to_string();
   let cmd_name = String::from("get-java-basic-types");
-  match run(field_type_kind) {
+  match run(basic_type_kind) {
     Ok(types) => Response::success(cmd_name, cwd_string, types),
     Err(error_msg) => Response::error(cmd_name, cwd_string, error_msg),
   }
